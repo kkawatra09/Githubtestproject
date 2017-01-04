f11144f32c74ebd2c6a9c038d16fb0e25c0c02a4
Kshitij Kawatra <kshitijkawatra@kshitijs-mbp.attlocal.net>
2017-01-03 21:56:48 -0800

    Initial Commit

---
+++ HelloTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloTheWorld
+//
+//  Created by Kshitij Kawatra on 1/3/17.
+//  Copyright © 2017 JAK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
